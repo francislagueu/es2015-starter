ab1d8d5157ecebe584d7f3fce55fa2e52980c16d
francislagueu <lagson6@yahoo.fr>
2018-11-26 13:22:28 -0800

    refactored the env file

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 mongoose.Promise = global.Promise;
-const MONGO_URL = 'mongodb://localhost:27017/boiler';
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -25,7 +25,7 @@ app.use('/users', UserRouter);
 app.use(NotFoundError);
 app.use(ErrorHandler);
 
-mongoose.connect(MONGO_URL, {useNewUrlParser: true}).then(()=>{
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useCreateIndex: true}).then(()=>{
     console.log('Database connected successtully...');
     app.listen(PORT, ()=>{
         console.log(`Application listening on http://localhost:${PORT}`);
